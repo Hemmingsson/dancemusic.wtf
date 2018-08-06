7884d76e6e83e8e5eff592dbfb400c599b388505
Mattias <hemmingsson.m@gmail.com>
2018-08-06 19:31:52 +0200

    flexbox detection + meta application name

--- src/js/index.js
+++ src/js/index.js
@@ -7,19 +7,25 @@ import mobile from './mobile.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   interactions.init()
-
-  // Detect if flexbox is supported
-  const d = document
-  let c = ' '
-  const prefixFlex = 'flex -webkit-flex -ms-flexbox -moz-box -webkit-box'.split(' ')
-  const elem = d.createElement('b')
-  let mStyle = elem.style
-  try {
-    for (var i = 0, len = prefixFlex.length; i < len; i++) {
-      c += ((mStyle.display = prefixFlex[i]) && mStyle.display !== prefixFlex[i]) ? 'no-flex' : 'flex', (i = len)
-    }
-  } catch (e) {
-    c += 'no-flex'
-  }
-  d.body.className = c + 'box'
+  if (!cssPropertySupported(['-ms-flexbox', '-webkit-box', 'flex'])) document.body.className = 'no-flex'
 })
+
+function cssPropertySupported (pNames) {
+  const element = document.createElement('a')
+
+  let index = pNames.length
+
+  try {
+    while (index--) {
+      const name = pNames[index]
+
+      element.style.display = name
+      if (element.style.display === name) {
+        return true
+      }
+    }
+  } catch (pError) {}
+
+  return false
+}
+
